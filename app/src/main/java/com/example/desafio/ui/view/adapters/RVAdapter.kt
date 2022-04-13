fe5ce95d961dc490e8a22d1e77a881217256b012
f.sanhueza.castro <francisco.j.s.c@hotmail.com>
2022-04-13 13:13:04 -0400

    RVAdapter.kt was changed, the setting of the view's now is made in the viewholder

--- app/src/main/java/com/example/desafio/ui/view/adapters/RVAdapter.kt
+++ app/src/main/java/com/example/desafio/ui/view/adapters/RVAdapter.kt
@@ -13,85 +13,102 @@ import com.example.desafio.data.model.RepositoriesModel
 import com.example.desafio.R
 
 
-class RVAdapter(var userList: List<RepositoriesModel>, private val ctx: Context,
-                val RepositoryOnClickListener: OnRepoClickListener
-): RecyclerView.Adapter<RVAdapter.MyViewHolder>() {
+class RVAdapter(
+    val repoList: List<RepositoriesModel>, private val ctx: Context,
+    val onRepoClickListener: OnRepoClickListener
+) : RecyclerView.Adapter<RVAdapter.MyViewHolder>() {
 
 
-interface OnRepoClickListener{
-    fun onRepoClick(owner: String, full_name: String)
-}
+    interface OnRepoClickListener {
+        fun onRepoClick(owner: String, full_name: String)
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-       val inflater = LayoutInflater.from(parent.context).inflate(R.layout.rvrow,parent,false)
+        val inflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(inflater.inflate(R.layout.rvrow, parent, false))
+    }
 
-        return MyViewHolder(inflater)
+    override fun getItemCount(): Int {
+        return repoList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       val datos: RepositoriesModel = userList[position]
+        holder.render(repoList[position])
+
+        holder.itemView.setOnClickListener {
+            onRepoClickListener.onRepoClick(
+                repoList[position].userModel.login,
+                repoList[position].name
+            )
+        }
+       /* val datos: RepositoriesModel = userList[position]
         holder.Tittle.text = datos.name
         holder.Descripcion.text = datos.description
         holder.Fork.text = datos.forks_count
         holder.Star.text = datos.stargazers_count
         holder.Usser.text = datos.userModel.login
-        holder.itemView.setOnClickListener{RepositoryOnClickListener.onRepoClick(datos.userModel.login, datos.name)}
-       val urlimage = datos.userModel.name
-            Glide.with(holder.image)
-                .load(urlimage)
-                .circleCrop()
-                .into(holder.image)
-
-        /*holder.rvView.setOnClickListener(object :View.OnClickListener{
-            override fun onClick(v: View?) {
-
-
-            }
-
-        })*/
-
+        holder.itemView.setOnClickListener {
+            RepositoryOnClickListener.onRepoClick(
+                datos.userModel.login,
+                datos.name
+            )
         }
+        val urlimage = datos.userModel.name
+        Glide.with(holder.image)
+            .load(urlimage)
+            .circleCrop()
+            .into(holder.image)*/
 
-
-        //holder.SobrUsser.text = datos.sobrenombre
-
-
-
-
-
-    override fun getItemCount(): Int {
-        return userList.size
     }
 
-    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
 
 
-        val Tittle: TextView
-        val Descripcion: TextView
-        val Fork: TextView
-        val Star: TextView
-        val Usser: TextView
-        val image: ImageView
+    class MyViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+        fun render(repositoriesModel: RepositoriesModel) {
+
+            val tittle = itemView.findViewById<View>(R.id.Tittle) as TextView
+            val descripcion = itemView.findViewById<View>(R.id.Description) as TextView
+            val fork = itemView.findViewById<View>(R.id.forks) as TextView
+            val star = itemView.findViewById<View>(R.id.stars) as TextView
+            val usser = itemView.findViewById<View>(R.id.nmbreusser) as TextView
+            val image = itemView.findViewById<View>(R.id.usserimage) as ImageView
+           // val rv = itemView.findViewById<View>(R.id.recyclerview) as RecyclerView
+
+            tittle.text = repositoriesModel.name
+            descripcion.text = repositoriesModel.description
+            fork.text = repositoriesModel.forks_count
+            star.text = repositoriesModel.stargazers_count
+            usser.text = repositoriesModel.userModel.login
+            Glide.with(image)
+                .load(repositoriesModel.userModel.name)
+                .circleCrop()
+                .into(image)
 
 
-        //val SobrUsser: TextView
-
-        init {
-            Tittle = itemView.findViewById<View>(R.id.Tittle) as TextView
-            Descripcion = itemView.findViewById<View>(R.id.Description) as TextView
-            Fork = itemView.findViewById<View>(R.id.forks) as TextView
-            Star = itemView.findViewById<View>(R.id.stars) as TextView
-            Usser = itemView.findViewById<View>(R.id.nmbreusser) as TextView
-            image = itemView.findViewById<View>(R.id.usserimage) as ImageView
-
-
-
-            //SobrUsser = itemView.findViewById<View>(R.id.sobrenmbreusser) as TextView
         }
 
 
 
+
+        /*  val Tittle: TextView
+          val Descripcion: TextView
+          val Fork: TextView
+          val Star: TextView
+          val Usser: TextView
+          val image: ImageView
+
+
+          init {
+              Tittle = itemView.findViewById<View>(R.id.Tittle) as TextView
+              Descripcion = itemView.findViewById<View>(R.id.Description) as TextView
+              Fork = itemView.findViewById<View>(R.id.forks) as TextView
+              Star = itemView.findViewById<View>(R.id.stars) as TextView
+              Usser = itemView.findViewById<View>(R.id.nmbreusser) as TextView
+              image = itemView.findViewById<View>(R.id.usserimage) as ImageView
+
+          }*/
 
     }
 
